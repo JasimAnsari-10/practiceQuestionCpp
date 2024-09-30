c43b0596041850ac10daeded70e92f4dcbee883b
Jasim Ansari <itsjasim.ansari@gmail.com>
2024-10-01 01:42:17 +0530

    cpp

---
+++ sumDivBy3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int sum = 0;
+    cout<<"enter the number: ";
+    cin>>n;
+    for(int i=0; i<=n;i++){
+        if(i%3==0){
+        sum +=i;
+        i++;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
